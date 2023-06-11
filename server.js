1c16d6f58c0d900b4c3365b89cc13b4f3fbfaa54
madison-mitchell <missmadisonrose@icloud.com>
2023-06-11 00:02:35 -0400

    Added madison-mitchell.github.io as part of CORS

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.setHeader('Access-Control-Allow-Origin', 'https://madison-mitchell.github.io');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
